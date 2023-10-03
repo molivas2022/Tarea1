6b86485e0dd1caa1fe71bead423c07e05a99c484
molivas2022 <molivas2022@inf.udec.cl>
2023-10-03 11:03:45 -0300

    add: Atributos y metodos de clase Efectivo

--- src/pagos/Efectivo.java
+++ src/pagos/Efectivo.java
@@ -1,4 +1,15 @@
 package pagos;
 
 public class Efectivo extends Pago {
+    private float ingreso;
+    public Efectivo(float con_cuanto_se_paga, float cuanto_se_quiere_pagar) {
+        super();
+        super.set_monto(cuanto_se_quiere_pagar);
+        this.ingreso = con_cuanto_se_paga;
+
+        //FALTA CONSIDERAR CASO INGRESO < MONTO
+    }
+    public float calcDevolucion() {
+        return super.get_monto() - ingreso;
+    }
 }
