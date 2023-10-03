cb47a926abad5d65269cb07a55645208f68fee53
molivas2022 <molivas2022@inf.udec.cl>
2023-10-03 11:01:05 -0300

    add: Atributos a pago y metodos setter/getter de monto

--- src/pagos/Pago.java
+++ src/pagos/Pago.java
@@ -1,4 +1,13 @@
 package pagos;
+import java.util.Date;
 
 public abstract class Pago {
+    private float monto;
+    private Date fecha;
+    public float get_monto() {
+        return monto;
+    }
+    protected void set_monto(float monto) {
+        this.monto = monto;
+    }
 }
