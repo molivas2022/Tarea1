0bfbb6d986d34a02c4eac96044790a11444a956b
molivas2022 <molivas2022@inf.udec.cl>
2023-10-03 11:04:29 -0300

    add: Atributos y metodos de clase Transferencia

--- src/pagos/Transferencia.java
+++ src/pagos/Transferencia.java
@@ -1,4 +1,18 @@
 package pagos;
 
 public class Transferencia extends Pago {
+    private String banco;
+    private String numCuenta;
+    public Transferencia(float monto, String banco, String numCuenta) {
+        super();
+        super.set_monto(monto);
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+    public String get_banco() {
+        return banco;
+    }
+    public String get_numCuenta() {
+        return numCuenta;
+    }
 }
