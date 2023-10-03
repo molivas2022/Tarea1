ca5f67befe1d5ea1e778990c62a7305c212aaf73
molivas2022 <molivas2022@inf.udec.cl>
2023-10-03 11:14:31 -0300

    add: Atributos y metodos de clase Tarjeta

--- src/pagos/Tarjeta.java
+++ src/pagos/Tarjeta.java
@@ -1,4 +1,18 @@
 package pagos;
 
 public class Tarjeta extends Pago {
+    private String tipo;
+    private String numTransaccion; //No le coloqué la tilde
+    public Tarjeta(float monto, String numTransaccion, String tipo) {
+        super();
+        super.set_monto(monto);
+        this.numTransaccion = numTransaccion;
+        this.tipo = tipo;
+    }
+    public String get_tipo() {
+        return tipo;
+    }
+    public String get_numTransaccion() {
+        return numTransaccion;
+    }
 }
