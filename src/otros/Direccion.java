57fcffc70d6fbbe410c3ab23fc5aae3c1c495012
molivas2022 <molivas2022@inf.udec.cl>
2023-10-04 22:07:40 -0300

    add: Implementación clase Dirección

---
+++ src/otros/Direccion.java
@@ -0,0 +1,11 @@
+package otros;
+
+public class Direccion {
+    private String direccion;
+    public Direccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String get_direccion() {
+        return direccion;
+    }
+}
